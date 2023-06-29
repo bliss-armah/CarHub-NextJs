3a3ffb6051b9fa8246c2b70bbd324a8cb45a760d
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-06-29 15:03:36 +0000

    comboboc designed and functioning

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,8 @@
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
-export {Hero,CustomButton,Footer,Navbar
-}
+export { Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter, };
