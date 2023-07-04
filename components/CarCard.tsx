90a46eb426c58b1ff50badcc485e7c14e9c33be9
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-07-04 15:23:55 +0000

    single car modal created

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import { CarProps } from "@/types";
 import Image from "next/image";
 import React, { useState } from "react";
 import CustomButton from "./CustomButton";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import CarDetails from "./CarDetails";
 
 interface CarCardProps {
@@ -31,7 +32,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
