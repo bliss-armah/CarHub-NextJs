7f06fea5cf4a3dd3136dd07003c3e7a3362863e1
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-07-14 11:17:05 +0000

    client side code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,48 @@
+"use client";
+
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { FilterProps } from "@/types";
 import { fetchCars } from "@/utils";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default async function Home({ searchParams }:{searchParams:FilterProps}) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
+export default function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // search states
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // filter states
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+
+  // pagination
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer || "",
+        year: year || 2022,
+        fuel: fuel || "",
+        limit: limit || 10,
+        model: model || "",
+      });
+      setAllCars(result);
+    } catch (error) {
+      console.error();
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  useEffect(() => {
+    getCars();
+  }, [fuel, year, limit, manufacturer, model]);
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -23,25 +55,42 @@ export default async function Home({ searchParams }:{searchParams:FilterProps}) 
           <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
-          <SearchBar />
+          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter
+              title="fuel"
+              options={fuels}
+              setFilter={setFuel}
+              setFilter={setYear}
+            />
             <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
-        {!isDataEmpty ? (
+        {allCars?.length > 0 ? (
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
             </div>
+            {loading &&(
+              <div className="mt-16 w-full flex-center">
+                <Image
+                src="/loader.svg"
+                alt="loader"
+                width={50}
+                height={50}
+                className="object-contain"
+                />
+              </div>
+            )}
             <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit={setLimit}
             />
-          </section> 
+          </section>
         ) : (
           <div className="home__erro-container">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
